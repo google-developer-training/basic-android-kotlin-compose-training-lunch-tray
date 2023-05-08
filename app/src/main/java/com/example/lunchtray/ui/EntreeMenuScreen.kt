5e8e780cfb96435e4878375c898504fe310c2607
John Shea <sheajohn@google.com>
2023-05-08 16:49:54 -0400

    Initial main_m3 branch commit

---
+++ app/src/main/java/com/example/lunchtray/ui/EntreeMenuScreen.kt
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.lunchtray.ui
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.lunchtray.R
+import com.example.lunchtray.datasource.DataSource
+import com.example.lunchtray.model.MenuItem
+import com.example.lunchtray.model.MenuItem.EntreeItem
+
+@Composable
+fun EntreeMenuScreen(
+    options: List<EntreeItem>,
+    onCancelButtonClicked: () -> Unit,
+    onNextButtonClicked: () -> Unit,
+    onSelectionChanged: (EntreeItem) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    BaseMenuScreen(
+        options = options,
+        onCancelButtonClicked = onCancelButtonClicked,
+        onNextButtonClicked = onNextButtonClicked,
+        onSelectionChanged = onSelectionChanged as (MenuItem) -> Unit,
+        modifier = modifier
+    )
+}
+
+@Preview
+@Composable
+fun EntreeMenuPreview(){
+    EntreeMenuScreen(
+        options = DataSource.entreeMenuItems,
+        onCancelButtonClicked = {},
+        onNextButtonClicked = {},
+        onSelectionChanged = {},
+        modifier = Modifier
+            .padding(dimensionResource(R.dimen.padding_medium))
+            .verticalScroll(rememberScrollState())
+    )
+}
